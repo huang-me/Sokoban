e8ec092a036713bb6e38165d5d178fdeb53e3356
huang-me <amos0107@gmail.com>
2020-04-19 02:33:03 +0800

    done all basic feature, need to finish bomb and make level2 start when level1 passed.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,71 +2,24 @@
 #include "ui_mainwindow.h"
 #include <QPixmap>
 #include <iostream>
+#include <QAction>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->congrates->setVisible(false);
-
-    // import images
-    QPixmap background("/home/pd2/Desktop/2020-pd2-sokoban/imgs/back.jpg");
-    QPixmap player_pic("/home/pd2/Desktop/2020-pd2-sokoban/imgs/player_front.png");
-    QPixmap movable("/home/pd2/Desktop/2020-pd2-sokoban/imgs/movable.png");
-    QPixmap unmovable("/home/pd2/Desktop/2020-pd2-sokoban/imgs/unmovable.png");
-    QPixmap end_pic("/home/pd2/Desktop/2020-pd2-sokoban/imgs/endPoint.png");
-
-    // set up the background size and position
-    ui->label_bg->setPixmap(background.scaled(480,540));
-    ui->player->setPixmap(player_pic.scaled(60,60));
-
-    // put the player in the initial position
-    player_x = 120;
-    player_y = 120;
-    ui->player->setGeometry(player_x,player_x,60,60);
 
     // unable the buttons
     ui->btns->setEnabled(false);
 
-    // put movable boxes initial place
-    ui->box1->setPixmap(movable.scaled(60,60));
-    ui->box1->setGeometry(180,120,60,60);
-    ui->box2->setPixmap(movable.scaled(60,60));
-    ui->box2->setGeometry(240,180,60,60);
-    ui->box3->setPixmap(movable.scaled(60,60));
-    ui->box3->setGeometry(240,240,60,60);
-    ui->box4->setPixmap(movable.scaled(60,60));
-    ui->box4->setGeometry(180,360,60,60);
-    ui->box5->setPixmap(movable.scaled(60,60));
-    ui->box5->setGeometry(60,360,60,60);
-    ui->box6->setPixmap(movable.scaled(60,60));
-    ui->box6->setGeometry(240,360,60,60);
-    ui->box7->setPixmap(movable.scaled(60,60));
-    ui->box7->setGeometry(300,360,60,60);
+    set_level1();
 
-    // set end point
-    ui->end1->setPixmap(end_pic.scaled(20,20));
-    ui->end1->setGeometry(60+20,120+20,20,20);
-    ui->end2->setPixmap(end_pic.scaled(20,20));
-    ui->end2->setGeometry(60+20,240+20,20,20);
-    ui->end3->setPixmap(end_pic.scaled(20,20));
-    ui->end3->setGeometry(180+20,360+20,20,20);
-    ui->end4->setPixmap(end_pic.scaled(20,20));
-    ui->end4->setGeometry(240+20,300+20,20,20);
-    ui->end5->setPixmap(end_pic.scaled(20,20));
-    ui->end5->setGeometry(240+20,420+20,20,20);
-    ui->end6->setPixmap(end_pic.scaled(20,20));
-    ui->end6->setGeometry(300+20,180+20,20,20);
-    ui->end7->setPixmap(end_pic.scaled(20,20));
-    ui->end7->setGeometry(360+20,360+20,20,20);
-
-    // put unmovable boxes where they should be
-    for(int i=0; i<len_unmove; i++) {
-        QLabel *tmp = new QLabel(ui->blocks);
-        tmp->setPixmap(unmovable.scaled(60,60));
-        tmp->setGeometry(unmove[i][0]*60,unmove[i][1]*60,60,60);
-    }
+    // connect menu buttons
+    connect(ui->actionStart_Game,SIGNAL(triggered()),this,SLOT(set_level1()));
+    connect(ui->Level1,SIGNAL(triggered()),this,SLOT(set_level1()));
+    connect(ui->Level2,SIGNAL(triggered()),this,SLOT(set_level2()));
+    connect(ui->actionQuit_Game,SIGNAL(triggered()),this,SLOT(restartLevel()));
 }
 
 MainWindow::~MainWindow()
@@ -75,15 +28,14 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::keyPressEvent(QKeyEvent *e) {
+    // if key_esc pressed, restart the game
+    if(e->key() == Qt::Key_Escape) {
+        restartLevel();
+    }
+
     // if the game haven't done
     if(player_x > 0 && player_y > 0) {
         int tmpx = player_x, tmpy = player_y;
-        int memo[10][2];
-        // memorize the movable location
-        for(int j=0;j<len_move;j++) {
-            memo[j][0] = move[j][0];
-            memo[j][1] = move[j][1];
-        }
 
         if(e->key() == Qt::Key_Right) {
             player_x += 60; // move right
@@ -231,4 +183,332 @@ void MainWindow::showfinish() {
     player_y = -1;
     ui->congrates->setVisible(true);
     ui->game->setEnabled(false);
+    ui->unmove->setEnabled(false);
+}
+
+void MainWindow::set_level1() {
+    // initialize constants
+    len_move = level1_len_move;
+    len_unmove = level1_len_unmove;
+    step_count = 0;
+    level_num = 1;
+    bomb_x = level1_bombx;
+    bomb_y = level1_bomby;
+
+    ui->congrates->setVisible(false);
+    ui->game->setEnabled(true);
+    ui->steps->setNum(step_count);
+
+    // import images
+    QPixmap background("/home/pd2/Desktop/2020-pd2-sokoban/imgs/back.jpg");
+    QPixmap player_pic("/home/pd2/Desktop/2020-pd2-sokoban/imgs/player_front.png");
+    QPixmap movable("/home/pd2/Desktop/2020-pd2-sokoban/imgs/movable.png");
+    QPixmap unmovable("/home/pd2/Desktop/2020-pd2-sokoban/imgs/unmovable.png");
+    QPixmap end_pic("/home/pd2/Desktop/2020-pd2-sokoban/imgs/endPoint.png");
+    QPixmap bomb("/home/pd2/Desktop/2020-pd2-sokoban/imgs/bomb.png");
+
+    // setup the bomb
+    ui->bomb->setPixmap(bomb.scaled(60,60));
+    ui->bomb->setGeometry(bomb_x,bomb_y,60,60);
+
+    // set up the background size and position
+    ui->label_bg->setPixmap(background.scaled(480,540));
+    ui->player->setPixmap(player_pic.scaled(60,60));
+
+    // put the player in the initial position
+    player_x = 120;
+    player_y = 120;
+    ui->player->setGeometry(player_x,player_y,60,60);
+    ui->player->setEnabled(true);
+
+    // put movable boxes initial place
+    ui->box1->setPixmap(movable.scaled(60,60));
+    ui->box1->setGeometry(180,120,60,60);
+    ui->box2->setPixmap(movable.scaled(60,60));
+    ui->box2->setGeometry(240,180,60,60);
+    ui->box3->setPixmap(movable.scaled(60,60));
+    ui->box3->setGeometry(240,240,60,60);
+    ui->box4->setPixmap(movable.scaled(60,60));
+    ui->box4->setGeometry(180,360,60,60);
+    ui->box5->setPixmap(movable.scaled(60,60));
+    ui->box5->setGeometry(60,360,60,60);
+    ui->box6->setPixmap(movable.scaled(60,60));
+    ui->box6->setGeometry(240,360,60,60);
+    ui->box7->setPixmap(movable.scaled(60,60));
+    ui->box7->setGeometry(300,360,60,60);
+
+    // set end point
+    ui->end1->setPixmap(end_pic.scaled(20,20));
+    ui->end1->setGeometry(60+20,120+20,20,20);
+    ui->end1->setVisible(true);
+    ui->end2->setPixmap(end_pic.scaled(20,20));
+    ui->end2->setGeometry(60+20,240+20,20,20);
+    ui->end2->setVisible(true);
+    ui->end3->setPixmap(end_pic.scaled(20,20));
+    ui->end3->setGeometry(180+20,360+20,20,20);
+    ui->end3->setVisible(true);
+    ui->end4->setPixmap(end_pic.scaled(20,20));
+    ui->end4->setGeometry(240+20,300+20,20,20);
+    ui->end4->setVisible(true);
+    ui->end5->setPixmap(end_pic.scaled(20,20));
+    ui->end5->setGeometry(240+20,420+20,20,20);
+    ui->end5->setVisible(true);
+    ui->end6->setPixmap(end_pic.scaled(20,20));
+    ui->end6->setGeometry(300+20,180+20,20,20);
+    ui->end6->setVisible(true);
+    ui->end7->setPixmap(end_pic.scaled(20,20));
+    ui->end7->setGeometry(360+20,360+20,20,20);
+    ui->end7->setVisible(true);
+
+    // put unmovable boxes where they should be
+    for(int i=0; i<len_unmove; i++) {
+        unmove[i][0] = level1_unmove[i][0];
+        unmove[i][1] = level1_unmove[i][1];
+    }
+    ui->unmove1->setPixmap(unmovable.scaled(60,60));
+    ui->unmove1->setGeometry(unmove[0][0]*60,unmove[0][1]*60,60,60);
+    ui->unmove2->setPixmap(unmovable.scaled(60,60));
+    ui->unmove2->setGeometry(unmove[1][0]*60,unmove[1][1]*60,60,60);
+    ui->unmove3->setPixmap(unmovable.scaled(60,60));
+    ui->unmove3->setGeometry(unmove[2][0]*60,unmove[2][1]*60,60,60);
+    ui->unmove4->setPixmap(unmovable.scaled(60,60));
+    ui->unmove4->setGeometry(unmove[3][0]*60,unmove[3][1]*60,60,60);
+    ui->unmove5->setPixmap(unmovable.scaled(60,60));
+    ui->unmove5->setGeometry(unmove[4][0]*60,unmove[4][1]*60,60,60);
+    ui->unmove6->setPixmap(unmovable.scaled(60,60));
+    ui->unmove6->setGeometry(unmove[5][0]*60,unmove[5][1]*60,60,60);
+    ui->unmove7->setPixmap(unmovable.scaled(60,60));
+    ui->unmove7->setGeometry(unmove[6][0]*60,unmove[6][1]*60,60,60);
+    ui->unmove8->setPixmap(unmovable.scaled(60,60));
+    ui->unmove8->setGeometry(unmove[7][0]*60,unmove[7][1]*60,60,60);
+    ui->unmove9->setPixmap(unmovable.scaled(60,60));
+    ui->unmove9->setGeometry(unmove[8][0]*60,unmove[8][1]*60,60,60);
+    ui->unmove10->setPixmap(unmovable.scaled(60,60));
+    ui->unmove10->setGeometry(unmove[9][0]*60,unmove[9][1]*60,60,60);
+    ui->unmove11->setPixmap(unmovable.scaled(60,60));
+    ui->unmove11->setGeometry(unmove[10][0]*60,unmove[10][1]*60,60,60);
+    ui->unmove12->setPixmap(unmovable.scaled(60,60));
+    ui->unmove12->setGeometry(unmove[11][0]*60,unmove[11][1]*60,60,60);
+    ui->unmove13->setPixmap(unmovable.scaled(60,60));
+    ui->unmove13->setGeometry(unmove[12][0]*60,unmove[12][1]*60,60,60);
+    ui->unmove14->setPixmap(unmovable.scaled(60,60));
+    ui->unmove14->setGeometry(unmove[13][0]*60,unmove[13][1]*60,60,60);
+    ui->unmove15->setPixmap(unmovable.scaled(60,60));
+    ui->unmove15->setGeometry(unmove[14][0]*60,unmove[14][1]*60,60,60);
+    ui->unmove16->setPixmap(unmovable.scaled(60,60));
+    ui->unmove16->setGeometry(unmove[15][0]*60,unmove[15][1]*60,60,60);
+    ui->unmove17->setPixmap(unmovable.scaled(60,60));
+    ui->unmove17->setGeometry(unmove[16][0]*60,unmove[16][1]*60,60,60);
+    ui->unmove18->setPixmap(unmovable.scaled(60,60));
+    ui->unmove18->setGeometry(unmove[17][0]*60,unmove[17][1]*60,60,60);
+    ui->unmove19->setPixmap(unmovable.scaled(60,60));
+    ui->unmove19->setGeometry(unmove[18][0]*60,unmove[18][1]*60,60,60);
+    ui->unmove20->setPixmap(unmovable.scaled(60,60));
+    ui->unmove20->setGeometry(unmove[19][0]*60,unmove[19][1]*60,60,60);
+    ui->unmove21->setPixmap(unmovable.scaled(60,60));
+    ui->unmove21->setGeometry(unmove[20][0]*60,unmove[20][1]*60,60,60);
+    ui->unmove22->setPixmap(unmovable.scaled(60,60));
+    ui->unmove22->setGeometry(unmove[21][0]*60,unmove[21][1]*60,60,60);
+    ui->unmove23->setPixmap(unmovable.scaled(60,60));
+    ui->unmove23->setGeometry(unmove[22][0]*60,unmove[22][1]*60,60,60);
+    ui->unmove24->setPixmap(unmovable.scaled(60,60));
+    ui->unmove24->setGeometry(unmove[23][0]*60,unmove[23][1]*60,60,60);
+    ui->unmove25->setPixmap(unmovable.scaled(60,60));
+    ui->unmove25->setGeometry(unmove[24][0]*60,unmove[24][1]*60,60,60);
+    ui->unmove26->setPixmap(unmovable.scaled(60,60));
+    ui->unmove26->setGeometry(unmove[25][0]*60,unmove[25][1]*60,60,60);
+    ui->unmove27->setPixmap(unmovable.scaled(60,60));
+    ui->unmove27->setGeometry(unmove[26][0]*60,unmove[26][1]*60,60,60);
+    ui->unmove28->setPixmap(unmovable.scaled(60,60));
+    ui->unmove28->setGeometry(unmove[27][0]*60,unmove[27][1]*60,60,60);
+    ui->unmove29->setPixmap(unmovable.scaled(60,60));
+    ui->unmove29->setGeometry(unmove[28][0]*60,unmove[28][1]*60,60,60);
+    ui->unmove30->setPixmap(unmovable.scaled(60,60));
+    ui->unmove30->setGeometry(unmove[29][0]*60,unmove[29][1]*60,60,60);
+    ui->unmove31->setPixmap(unmovable.scaled(60,60));
+    ui->unmove31->setGeometry(unmove[30][0]*60,unmove[30][1]*60,60,60);
+    ui->unmove32->setPixmap(unmovable.scaled(60,60));
+    ui->unmove32->setGeometry(unmove[31][0]*60,unmove[31][1]*60,60,60);
+    ui->unmove33->setPixmap(unmovable.scaled(60,60));
+    ui->unmove33->setGeometry(unmove[32][0]*60,unmove[32][1]*60,60,60);
+    ui->unmove34->setPixmap(unmovable.scaled(60,60));
+    ui->unmove34->setGeometry(unmove[33][0]*60,unmove[33][1]*60,60,60);
+    ui->unmove35->setPixmap(unmovable.scaled(60,60));
+    ui->unmove35->setGeometry(unmove[34][0]*60,unmove[34][1]*60,60,60);
+
+    // set the value of movable boxes
+    for(int i=0;i<len_move;i++) {
+        move[i][0] = level1_move[i][0];
+        end[i][0] = level1_end[i][0];
+        move[i][1] = level1_move[i][1];
+        end[i][1] = level1_end[i][1];
+    }
+}
+
+void MainWindow::set_level2() {
+
+    // initialize constants
+    len_move = level2_len_move;
+    len_unmove = level2_len_unmove;
+    step_count = 0;
+    level_num = 2;
+    bomb_x = level2_bombx;
+    bomb_y = level2_bomby;
+
+    ui->congrates->setVisible(false);
+    ui->game->setEnabled(true);
+    ui->unmove->setEnabled(true);
+    ui->steps->setNum(step_count);
+
+    // import images
+    QPixmap background("/home/pd2/Desktop/2020-pd2-sokoban/imgs/back.jpg");
+    QPixmap player_pic("/home/pd2/Desktop/2020-pd2-sokoban/imgs/player_front.png");
+    QPixmap movable("/home/pd2/Desktop/2020-pd2-sokoban/imgs/movable.png");
+    QPixmap unmovable("/home/pd2/Desktop/2020-pd2-sokoban/imgs/unmovable.png");
+    QPixmap end_pic("/home/pd2/Desktop/2020-pd2-sokoban/imgs/endPoint.png");
+    QPixmap bomb("/home/pd2/Desktop/2020-pd2-sokoban/imgs/bomb.png");
+
+    // set up the background size and position
+    ui->label_bg->setPixmap(background.scaled(480,540));
+    ui->player->setPixmap(player_pic.scaled(60,60));
+
+    // setup the bomb
+    ui->bomb->setPixmap(bomb.scaled(60,60));
+    ui->bomb->setGeometry(bomb_x,bomb_y,60,60);
+
+    // put the player in the initial position
+    player_x = 240;
+    player_y = 420;
+    ui->player->setGeometry(player_x,player_y,60,60);
+    ui->player->setEnabled(true);
+
+    // put movable boxes initial place
+    ui->box1->setPixmap(movable.scaled(60,60));
+    ui->box1->setGeometry(120,120,60,60);
+    ui->box2->setPixmap(movable.scaled(60,60));
+    ui->box2->setGeometry(120,240,60,60);
+    ui->box3->setPixmap(movable.scaled(60,60));
+    ui->box3->setGeometry(240,120,60,60);
+    ui->box4->setPixmap(movable.scaled(60,60));
+    ui->box4->setGeometry(300,240,60,60);
+    ui->box5->setPixmap(movable.scaled(60,60));
+    ui->box5->setGeometry(300,360,60,60);
+    ui->box6->setPixmap(movable.scaled(60,60));
+    ui->box6->setGeometry(240,360,60,60);
+    ui->box7->setPixmap(movable.scaled(60,60));
+    ui->box7->setGeometry(180,360,60,60);
+
+    // set end point
+    ui->end1->setPixmap(end_pic.scaled(20,20));
+    ui->end1->setGeometry(60+20,180+20,20,20);
+    ui->end1->setVisible(true);
+    ui->end2->setPixmap(end_pic.scaled(20,20));
+    ui->end2->setGeometry(60+20,300+20,20,20);
+    ui->end2->setVisible(true);
+    ui->end3->setPixmap(end_pic.scaled(20,20));
+    ui->end3->setGeometry(180+20,180+20,20,20);
+    ui->end3->setVisible(true);
+    ui->end4->setPixmap(end_pic.scaled(20,20));
+    ui->end4->setGeometry(180+20,300+20,20,20);
+    ui->end4->setVisible(true);
+    ui->end5->setPixmap(end_pic.scaled(20,20));
+    ui->end5->setGeometry(300+20,180+20,20,20);
+    ui->end5->setVisible(true);
+    ui->end6->setPixmap(end_pic.scaled(20,20));
+    ui->end6->setGeometry(300+20,300+20,20,20);
+    ui->end6->setVisible(true);
+    ui->end7->setPixmap(end_pic.scaled(20,20));
+    ui->end7->setGeometry(360+20,240+20,20,20);
+    ui->end7->setVisible(true);
+
+    // put unmovable boxes where they should be
+    for(int i=0; i<len_unmove; i++) {
+        unmove[i][0] = level2_unmove[i][0];
+        unmove[i][1] = level2_unmove[i][1];
+    }
+
+    ui->unmove1->setPixmap(unmovable.scaled(60,60));
+    ui->unmove1->setGeometry(unmove[0][0]*60,unmove[0][1]*60,60,60);
+    ui->unmove2->setPixmap(unmovable.scaled(60,60));
+    ui->unmove2->setGeometry(unmove[1][0]*60,unmove[1][1]*60,60,60);
+    ui->unmove3->setPixmap(unmovable.scaled(60,60));
+    ui->unmove3->setGeometry(unmove[2][0]*60,unmove[2][1]*60,60,60);
+    ui->unmove4->setPixmap(unmovable.scaled(60,60));
+    ui->unmove4->setGeometry(unmove[3][0]*60,unmove[3][1]*60,60,60);
+    ui->unmove5->setPixmap(unmovable.scaled(60,60));
+    ui->unmove5->setGeometry(unmove[4][0]*60,unmove[4][1]*60,60,60);
+    ui->unmove6->setPixmap(unmovable.scaled(60,60));
+    ui->unmove6->setGeometry(unmove[5][0]*60,unmove[5][1]*60,60,60);
+    ui->unmove7->setPixmap(unmovable.scaled(60,60));
+    ui->unmove7->setGeometry(unmove[6][0]*60,unmove[6][1]*60,60,60);
+    ui->unmove8->setPixmap(unmovable.scaled(60,60));
+    ui->unmove8->setGeometry(unmove[7][0]*60,unmove[7][1]*60,60,60);
+    ui->unmove9->setPixmap(unmovable.scaled(60,60));
+    ui->unmove9->setGeometry(unmove[8][0]*60,unmove[8][1]*60,60,60);
+    ui->unmove10->setPixmap(unmovable.scaled(60,60));
+    ui->unmove10->setGeometry(unmove[9][0]*60,unmove[9][1]*60,60,60);
+    ui->unmove11->setPixmap(unmovable.scaled(60,60));
+    ui->unmove11->setGeometry(unmove[10][0]*60,unmove[10][1]*60,60,60);
+    ui->unmove12->setPixmap(unmovable.scaled(60,60));
+    ui->unmove12->setGeometry(unmove[11][0]*60,unmove[11][1]*60,60,60);
+    ui->unmove13->setPixmap(unmovable.scaled(60,60));
+    ui->unmove13->setGeometry(unmove[12][0]*60,unmove[12][1]*60,60,60);
+    ui->unmove14->setPixmap(unmovable.scaled(60,60));
+    ui->unmove14->setGeometry(unmove[13][0]*60,unmove[13][1]*60,60,60);
+    ui->unmove15->setPixmap(unmovable.scaled(60,60));
+    ui->unmove15->setGeometry(unmove[14][0]*60,unmove[14][1]*60,60,60);
+    ui->unmove16->setPixmap(unmovable.scaled(60,60));
+    ui->unmove16->setGeometry(unmove[15][0]*60,unmove[15][1]*60,60,60);
+    ui->unmove17->setPixmap(unmovable.scaled(60,60));
+    ui->unmove17->setGeometry(unmove[16][0]*60,unmove[16][1]*60,60,60);
+    ui->unmove18->setPixmap(unmovable.scaled(60,60));
+    ui->unmove18->setGeometry(unmove[17][0]*60,unmove[17][1]*60,60,60);
+    ui->unmove19->setPixmap(unmovable.scaled(60,60));
+    ui->unmove19->setGeometry(unmove[18][0]*60,unmove[18][1]*60,60,60);
+    ui->unmove20->setPixmap(unmovable.scaled(60,60));
+    ui->unmove20->setGeometry(unmove[19][0]*60,unmove[19][1]*60,60,60);
+    ui->unmove21->setPixmap(unmovable.scaled(60,60));
+    ui->unmove21->setGeometry(unmove[20][0]*60,unmove[20][1]*60,60,60);
+    ui->unmove22->setPixmap(unmovable.scaled(60,60));
+    ui->unmove22->setGeometry(unmove[21][0]*60,unmove[21][1]*60,60,60);
+    ui->unmove23->setPixmap(unmovable.scaled(60,60));
+    ui->unmove23->setGeometry(unmove[22][0]*60,unmove[22][1]*60,60,60);
+    ui->unmove24->setPixmap(unmovable.scaled(60,60));
+    ui->unmove24->setGeometry(unmove[23][0]*60,unmove[23][1]*60,60,60);
+    ui->unmove25->setPixmap(unmovable.scaled(60,60));
+    ui->unmove25->setGeometry(unmove[24][0]*60,unmove[24][1]*60,60,60);
+    ui->unmove26->setPixmap(unmovable.scaled(60,60));
+    ui->unmove26->setGeometry(unmove[25][0]*60,unmove[25][1]*60,60,60);
+    ui->unmove27->setPixmap(unmovable.scaled(60,60));
+    ui->unmove27->setGeometry(unmove[26][0]*60,unmove[26][1]*60,60,60);
+    ui->unmove28->setPixmap(unmovable.scaled(60,60));
+    ui->unmove28->setGeometry(unmove[27][0]*60,unmove[27][1]*60,60,60);
+    ui->unmove29->setPixmap(unmovable.scaled(60,60));
+    ui->unmove29->setGeometry(unmove[28][0]*60,unmove[28][1]*60,60,60);
+    ui->unmove30->setPixmap(unmovable.scaled(60,60));
+    ui->unmove30->setGeometry(unmove[29][0]*60,unmove[29][1]*60,60,60);
+    ui->unmove31->setPixmap(unmovable.scaled(60,60));
+    ui->unmove31->setGeometry(unmove[30][0]*60,unmove[30][1]*60,60,60);
+    ui->unmove32->setPixmap(unmovable.scaled(60,60));
+    ui->unmove32->setGeometry(unmove[31][0]*60,unmove[31][1]*60,60,60);
+    ui->unmove33->setPixmap(unmovable.scaled(60,60));
+    ui->unmove33->setGeometry(unmove[32][0]*60,unmove[32][1]*60,60,60);
+    ui->unmove34->setPixmap(unmovable.scaled(60,60));
+    ui->unmove34->setGeometry(unmove[33][0]*60,unmove[33][1]*60,60,60);
+    ui->unmove35->setVisible(false);
+
+    // set the value of movable boxes
+    for(int i=0;i<len_move;i++) {
+        move[i][0] = level2_move[i][0];
+        end[i][0] = level2_end[i][0];
+        move[i][1] = level2_move[i][1];
+        end[i][1] = level2_end[i][1];
+    }
+}
+
+void MainWindow::restartLevel() {
+    if(level_num == 1)
+        set_level1();
+    else
+        set_level2();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -20,22 +20,50 @@ public:
     void showfinish();
     ~MainWindow();
 
+    // player position
     int player_x, player_y;
-    int len_unmove = 35;
-    int len_move = 7,step_count;
-    int unmove[50][2] = {{2,0},{3,0},{4,0},{5,0},{6,0},{6,1},{6,2},{6,3},{6,4},{6,5}, \
+    // level now
+    int level_num;
+    // boxes and steps now
+    int len_unmove, len_move, step_count;
+    int unmove[35][2], move[7][2], end[7][2];
+    // position of the bomb
+    int bomb_x, bomb_y;
+
+    // Level_1 map
+    int level1_len_unmove = 35, level1_len_move = 7;
+    int level1_unmove[35][2] = {{2,0},{3,0},{4,0},{5,0},{6,0},{6,1},{6,2},{6,3},{6,4},{6,5}, \
                          {7,5},{7,6},{7,7},{7,8},{6,8},{5,8},{4,8},{3,8},{2,8},{1,8}, \
                          {0,8},{0,7},{0,6},{0,5},{0,4},{0,3},{0,2},{0,1},{1,1},{2,1}, \
                          {1,3},{2,3},{2,4},{2,5},{3,4}
                         };
-    int move[10][2] = {{180,120},{240,180},{240,240},{180,360},{60,360},{240,360},{300,360}
+    int level1_move[7][2] = {{180,120},{240,180},{240,240},{180,360},{60,360},{240,360},{300,360}
                      };
-    int end[10][2] = {
+    int level1_end[7][2] = {
         {1,2},{1,4},{3,6},{4,5},{4,7},{5,3},{6,6}
     };
+    int level1_bombx = 360, level1_bomby = 420;
+
+    // Level_2 map
+    int level2_len_unmove = 34, level2_len_move = 7;
+    int level2_unmove[35][2] = {{0,0},{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{1,0},{1,6},{2,0}, \
+                                {2,3},{2,5},{2,6},{2,7},{2,8},{3,0},{3,8},{4,0},{4,3},{4,5}, \
+                                {4,8},{5,0},{5,8},{6,0},{6,1},{6,5},{6,6},{6,7},{6,8},{7,1}, \
+                                {7,2},{7,3},{7,4},{7,5}
+                        };
+    int level2_move[7][2] = {{120,120},{120,240},{240,120},{300,240},{300,360},{240,360},{180,360}
+                     };
+    int level2_end[7][2] = {
+        {1,3},{1,5},{3,3},{3,5},{5,3},{5,5},{6,4}
+    };
+    int level2_bombx = 360, level2_bomby = 120;
 
 signals:
 
+public slots:
+    void set_level1();
+    void set_level2();
+    void restartLevel();
 
 private:
     Ui::MainWindow *ui;
